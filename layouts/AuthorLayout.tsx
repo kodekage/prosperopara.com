715f3d61d29c36742b4e4ab151f378ed6e7ef04a
Prosper Opara <oparaprosper79@gmail.com>
2025-12-12 00:49:09 +0100

    chore: make text color dynamix

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -62,7 +62,7 @@ export default function AuthorLayout({ children, content }: Props) {
                           </div>
                           <div className="font-light">{date}</div>
                         </div>
-                        <div className="prose max-w-none pt-2 text-gray-500 dark:text-gray-400">
+                        <div className="max-w-none pt-2 text-gray-500 dark:text-gray-400">
                           <MDXLayoutRenderer code={body.code} />
                         </div>
 
